fecb3d930c974aba386658d5494114e0f6bba5f5
Rama Vasudevan <rvv@ornl.gov>
2024-09-13 17:24:09 -0400

    base microscope class change

--- DTMicroscope/base/microscope.py
+++ DTMicroscope/base/microscope.py
@@ -37,6 +37,87 @@ class BaseMicroscope(object):
         self.instrument_type = 'generic' #could be STEM, STM, AFM
         self.log = []] #microscope should have a log
 
+
+    def _parse_dataset(self):
+        """
+        Parses the dataset to identify and index different data types (IMAGE, SPECTRUM, POINT_CLOUD),
+        and processes the scan data accordingly.
+
+        TODO: Add ability to associate individual image datasets with spectroscopic datasets. 
+        This will need to be done by adding UIDs for each dataset and associating the spectroscopic dataset to the image
+        #in original_metadata, have a key for 'UID' and a key for 'associated-image' (the latter will only be for spectroscopic)
+        #or also have 'associated-spec' to refer to associated spectroscopic files
+        #When we read through the metadata we can immediately identify the available spectroscopic exps.
+        
+
+        This method creates three dictionaries to store indices for the different types of data:
+        - `_im_ind`: stores indices for IMAGE data.
+        - `_sp_ind`: stores indices for SPECTRUM data.
+        - `_pc_ind`: stores indices for POINT_CLOUD data.
+
+        It compiles the image data into a numpy array (`scan_ar`), extracts spatial coordinates from the first
+        image dataset, and places the scanning tip at the center of the scan.
+
+        Attributes:
+            self._im_ind (dict): Dictionary mapping IMAGE dataset keys to their respective indices.
+            self._sp_ind (dict): Dictionary mapping SPECTRUM dataset keys to their respective indices.
+            self._pc_ind (dict): Dictionary mapping POINT_CLOUD dataset keys to their respective indices.
+            self.scan_ar (numpy.ndarray): Array of computed image data.
+            self.x_coords (numpy.ndarray): Array of x-coordinates for spatial positioning.
+            self.y_coords (numpy.ndarray): Array of y-coordinates for spatial positioning.
+            self.x (float): x-coordinate of the scanning tip, placed at the center of the scan.
+            self.y (float): y-coordinate of the scanning tip, placed at the center of the scan.
+        """
+
+        _keys = list(self.dataset.keys())
+        #index dict
+        self._im_ind, self._sp_ind, self._pc_ind = {}, {}, {}
+        self.scan_ar = []
+
+        for i, (key, value) in enumerate(self.dataset.items()):
+            data_type = value.data_type
+
+            # Store indices and data based on the data type
+            if data_type == sd.DataType['IMAGE']:
+                self._im_ind[key] = i
+                self.scan_ar.append(value.compute())
+            elif data_type == sd.DataType['SPECTRUM']:
+                self._sp_ind[key] = i
+            elif data_type == sd.DataType['POINT_CLOUD']:
+                self._pc_ind[key] = i
+
+        # Convert the collected image data to a numpy array
+        if self.scan_ar:
+            try:
+                self.scan_ar = np.array(self.scan_ar)
+            except ValueError as e:
+                raise ValueError(
+                    "Inconsistent scan data. Ensure all scans have the same dimensions.") from e
+
+        #write spacial coordinates
+        first_im_ind = next(iter(self._im_ind))
+        self.x_coords = self.dataset[first_im_ind].x.values
+        self.y_coords = self.dataset[first_im_ind].y.values
+
+        self.x_min, self.x_max = self.x_coords.min(), self.x_coords.max()
+        self.y_min, self.y_max = self.y_coords.min(), self.y_coords.max()
+
+        #place tip in the center of scan
+        self.x = self.x_coords[len(self.x_coords)//2]
+        self.y = self.y_coords[len(self.y_coords)//2]
+
+    def get_dataset_info(self):
+        _keys = self.dataset.keys()
+        info_list = [
+            ('channels', list(_keys)),
+            ('signals', [self.dataset[k].quantity for k in _keys]),
+            ('units', [self.dataset[k].units for k in _keys]),
+            ('scans', list(self._im_ind.values())),
+            ('spectra', list(self._sp_ind.values())),
+            ('point_clouds', list(self._pc_ind.values())),
+        ]
+        return info_list
+    
     def setup_microscope(self):
         ###e.g., generate the data if required at this step
         return
