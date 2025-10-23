a954899e1011b2cc4cdcb807c4bb7097fb70d773
ahoust17 <88668350+ahoust17@users.noreply.github.com>
2025-10-23 07:30:39 -0400

    connect to ceos

--- DTMicroscope/base/stem.py
+++ DTMicroscope/base/stem.py
@@ -16,161 +16,156 @@ from skimage.draw import random_shapes
 
 # Extracted file ID from the provided URL
 
-class STEM(BaseMicroscope):
-    def __init__(self):
-        super().__init__()
-        self.name = 'STEM Microscope'
-        self.instrument_vendor = 'generic'
-        self.instrument_type = 'STEM'
-        self.data_source = 'None'  # enable people to provide it, generate it or use pre-acquired existing data
+# class STEM(BaseMicroscope):
+#     def __init__(self):
+#         super().__init__()
+#         self.name = 'STEM Microscope'
+#         self.instrument_vendor = 'generic'
+#         self.instrument_type = 'STEM'
+#         self.data_source = 'None'  # enable people to provide it, generate it or use pre-acquired existing data
+#         
+# 
+#     def _load_dataset(self, data_source = 'test.h5'):
+#         ###e.g., generate the data if required at this step
+#         reader = SciFiReaders.NSIDReader(data_source)
+#         self.dataset = reader.read()
+#         # reader.close()
+#         try:
+#             # Attempt to access keys attribute to check if dataset is already a dictionary
+#             dataset_keys = self.dataset.keys()
+# 
+#         except AttributeError:
+#             # AttributeError is raised because 'dataset' is a list, so convert it to a dictionary
+#             dataset_dict = {}
+#             for i, item in enumerate(self.dataset):
+#                 key = f"Channel_{i:03}"  # Format key with zero-padding, e.g., 'channel_000'
+#                 dataset_dict[key] = item
+#             self.dataset = dataset_dict
+#         print("Dataset loaded", self.dataset)
+#         return None
+# 
+#     def get_overview_image(self, channel_key = "Channel_000"):
+#         print("channel_key", channel_key)
+#         return np.array(self.dataset[channel_key])
+#         #return np.array(self.datasets[0])
+# 
+#     def _parse_dataset(self):
+#         """
+#         Parses the dataset to identify and index different data types (IMAGE, SPECTRUM, POINT_CLOUD),
+#         and processes the scan data accordingly.
+# 
+#         This method creates three dictionaries to store indices for the different types of data:
+#         - `_im_ind`: stores indices for IMAGE data.
+#         - `_sp_ind`: stores indices for SPECTRUM data.
+#         - `_pc_ind`: stores indices for POINT_CLOUD data.
+#         - `_spi_ind`: stores indices for SPECTRAL_IMAGE data.
+# 
+#         It compiles the image data into a numpy array (`scan_ar`), extracts spatial coordinates from the first
+#         image dataset, and places the scanning tip at the center of the scan.
+# 
+#         Attributes:
+#             self._im_ind (dict): Dictionary mapping IMAGE dataset keys to their respective indices.
+#             self._sp_ind (dict): Dictionary mapping SPECTRUM dataset keys to their respective indices.
+#             self._pc_ind (dict): Dictionary mapping POINT_CLOUD dataset keys to their respective indices.
+#             self.scan_ar (numpy.ndarray): Array of computed image data.
+#             self.x_coords (numpy.ndarray): Array of x-coordinates for spatial positioning.
+#             self.y_coords (numpy.ndarray): Array of y-coordinates for spatial positioning.
+#             self.x (float): x-coordinate of the scanning tip, placed at the center of the scan.
+#             self.y (float): y-coordinate of the scanning tip, placed at the center of the scan.
+#         """
+# 
+#         _keys = list(self.dataset.keys())
+#         #index dict
+#         self._im_ind, self._sp_ind, self._pc_ind, self._spi_ind = {}, {}, {}, {}
+#         self.scan_ar = []# scan area?
+# 
+#         for i, (key, value) in enumerate(self.dataset.items()):
+#             data_type = value.data_type
+# 
+#             # Store indices and data based on the data type
+#             if data_type == sid.DataType['IMAGE']:
+#                 self._im_ind[key] = i
+#                 self.scan_ar.append(value.compute())
+#             elif data_type == sid.DataType['SPECTRUM']:
+#                 self._sp_ind[key] = i
+#             elif data_type == sid.DataType['POINT_CLOUD']:
+#                 self._pc_ind[key] = i
+#             elif data_type == sid.DataType['SPECTRAL_IMAGE']:
+#                 self._spi_ind[key] = i
+# 
+# 
+# 
+#     def get_dataset_info(self):
+#         """
+#         TODO: add metadata info like: pixel size, accelaration voltage, convergence angle, etc.
+# 
+#         # so user can use this info to query the data
+#         """
+#         _keys = self.dataset.keys()
+#         info_list = [
+#             ('channel_keys', list(_keys)),
+#             ('signals', [self.dataset[k].quantity for k in _keys]),
+#             ('units', [self.dataset[k].units for k in _keys]),
+#             ('scans_channel_index', list(self._im_ind.values())), # eg: HAADF
+#             ('spectra_channel_index', list(self._sp_ind.values())), #
+#             ('point_clouds_channel_index', list(self._pc_ind.values())),
+#             ('spectral_image_channel_index', list(self._spi_ind.values())), # eg: EELS at each pixel in scan
+#             ('indexing', "image data['CHannel_000'] top left corner is (0,0)"),
+#             
+#         ]
+#         return info_list
+#     
+#     def get_point_data(self, spectrum_image_key, x, y):
+#         """emulates the data acquisition at a specific point
+# 
+#         Args:
+#             spectrum_image_key: Which index in sidpy dataset is spectrum index
+#             x : position in x
+#             y : position in y
+# 
+#         Returns:
+#             numpy array: data at that
+#             
+#         >>>spectrum = mic.datasets([1][0][0])
+#         >>>spectrum  is of shape 1496
+#         """
+#         return np.array(self.dataset[spectrum_image_key][x][y])
+#     
+#     
+#     def get_spectrum_image(self, spectrum_image_key="Channel_001"):
+#         """
+#         We need to calculate Errors for active learning experiments(DKL)
+#         Args:
+#             spectrum_image_index: Which index in sidpy dataset is spectrum index
+#             
+#         Returns: np.array -> shape is 3 dimensional
+#         """
+# 
+#         return np.array(self.dataset[spectrum_image_key])
+#     
+#     def get_spectrum_image_e_axis(self, spectrum_image_key = "Channel_001"):
+#         """
+#         To figure out scalarizers for active learning experiments(DKL). We need the energy range.
+#         TODO: This is more application oriented so need to think of a better structure
+#         Args:
+#             spectrum_image_index (str, optional)
+#         Returns: np.array
+#         """
+#         return self.dataset[spectrum_image_key].dim_2.values
+#     
+#         
+#     def pixel_size_X(self):
+#         return
+# 
+#     def pixel_size_Y(self):
+#         return 
+#     
+#     def get_acceleration_voltage(self):
+#         return 
         
 
-    def _load_dataset(self, data_source = 'test.h5'):
-        ###e.g., generate the data if required at this step
-        reader = SciFiReaders.NSIDReader(data_source)
-        self.dataset = reader.read()
-        # reader.close()
-        try:
-            # Attempt to access keys attribute to check if dataset is already a dictionary
-            dataset_keys = self.dataset.keys()
-
-        except AttributeError:
-            # AttributeError is raised because 'dataset' is a list, so convert it to a dictionary
-            dataset_dict = {}
-            for i, item in enumerate(self.dataset):
-                key = f"Channel_{i:03}"  # Format key with zero-padding, e.g., 'channel_000'
-                dataset_dict[key] = item
-            self.dataset = dataset_dict
-        print("Dataset loaded", self.dataset)
-        return None
-
-    def get_overview_image(self, channel_key = "Channel_000"):
-        print("channel_key", channel_key)
-        return np.array(self.dataset[channel_key])
-        #return np.array(self.datasets[0])
-
-    def _parse_dataset(self):
-        """
-        Parses the dataset to identify and index different data types (IMAGE, SPECTRUM, POINT_CLOUD),
-        and processes the scan data accordingly.
-
-        This method creates three dictionaries to store indices for the different types of data:
-        - `_im_ind`: stores indices for IMAGE data.
-        - `_sp_ind`: stores indices for SPECTRUM data.
-        - `_pc_ind`: stores indices for POINT_CLOUD data.
-        - `_spi_ind`: stores indices for SPECTRAL_IMAGE data.
-
-        It compiles the image data into a numpy array (`scan_ar`), extracts spatial coordinates from the first
-        image dataset, and places the scanning tip at the center of the scan.
-
-        Attributes:
-            self._im_ind (dict): Dictionary mapping IMAGE dataset keys to their respective indices.
-            self._sp_ind (dict): Dictionary mapping SPECTRUM dataset keys to their respective indices.
-            self._pc_ind (dict): Dictionary mapping POINT_CLOUD dataset keys to their respective indices.
-            self.scan_ar (numpy.ndarray): Array of computed image data.
-            self.x_coords (numpy.ndarray): Array of x-coordinates for spatial positioning.
-            self.y_coords (numpy.ndarray): Array of y-coordinates for spatial positioning.
-            self.x (float): x-coordinate of the scanning tip, placed at the center of the scan.
-            self.y (float): y-coordinate of the scanning tip, placed at the center of the scan.
-        """
-
-        _keys = list(self.dataset.keys())
-        #index dict
-        self._im_ind, self._sp_ind, self._pc_ind, self._spi_ind = {}, {}, {}, {}
-        self.scan_ar = []# scan area?
-
-        for i, (key, value) in enumerate(self.dataset.items()):
-            data_type = value.data_type
-
-            # Store indices and data based on the data type
-            if data_type == sid.DataType['IMAGE']:
-                self._im_ind[key] = i
-                self.scan_ar.append(value.compute())
-            elif data_type == sid.DataType['SPECTRUM']:
-                self._sp_ind[key] = i
-            elif data_type == sid.DataType['POINT_CLOUD']:
-                self._pc_ind[key] = i
-            elif data_type == sid.DataType['SPECTRAL_IMAGE']:
-                self._spi_ind[key] = i
-
-
-
-    def get_dataset_info(self):
-        """
-        TODO: add metadata info like: pixel size, accelaration voltage, convergence angle, etc.
-
-        # so user can use this info to query the data
-        """
-        _keys = self.dataset.keys()
-        info_list = [
-            ('channel_keys', list(_keys)),
-            ('signals', [self.dataset[k].quantity for k in _keys]),
-            ('units', [self.dataset[k].units for k in _keys]),
-            ('scans_channel_index', list(self._im_ind.values())), # eg: HAADF
-            ('spectra_channel_index', list(self._sp_ind.values())), #
-            ('point_clouds_channel_index', list(self._pc_ind.values())),
-            ('spectral_image_channel_index', list(self._spi_ind.values())), # eg: EELS at each pixel in scan
-            ('indexing', "image data['CHannel_000'] top left corner is (0,0)"),
-            
-        ]
-        return info_list
-    
-    def get_point_data(self, spectrum_image_key, x, y):
-        """emulates the data acquisition at a specific point
-
-        Args:
-            spectrum_image_key: Which index in sidpy dataset is spectrum index
-            x : position in x
-            y : position in y
-
-        Returns:
-            numpy array: data at that
-            
-        >>>spectrum = mic.datasets([1][0][0])
-        >>>spectrum  is of shape 1496
-        """
-        return np.array(self.dataset[spectrum_image_key][x][y])
-    
-    
-    def get_spectrum_image(self, spectrum_image_key="Channel_001"):
-        """
-        We need to calculate Errors for active learning experiments(DKL)
-        Args:
-            spectrum_image_index: Which index in sidpy dataset is spectrum index
-            
-        Returns: np.array -> shape is 3 dimensional
-        """
-
-        return np.array(self.dataset[spectrum_image_key])
-    
-    def get_spectrum_image_e_axis(self, spectrum_image_key = "Channel_001"):
-        """
-        To figure out scalarizers for active learning experiments(DKL). We need the energy range.
-        TODO: This is more application oriented so need to think of a better structure
-        Args:
-            spectrum_image_index (str, optional)
-        Returns: np.array
-        """
-        return self.dataset[spectrum_image_key].dim_2.values
-    
-        
-    def pixel_size_X(self):
-        return
-
-    def pixel_size_Y(self):
-        return 
-    
-    def get_acceleration_voltage(self):
-        return 
-        
-        
-    
-    
-    
-
-    
-# The DTSTEM and RealSTEM classes should be interchangeable in the notebooks
-class DTSTEM(BaseMicroscope):
+# The DTSTEM and smart_proxy classes should be interchangeable in the notebooks
+class DTSTEM_proxy(BaseMicroscope):
     def __init__(self, data_mode):
         super().__init__()
         if data_mode.lower() == 'simulation':
@@ -206,7 +201,7 @@ class DTSTEM(BaseMicroscope):
                                       'screen_current': None,
                                       'exposure_time': None}}
         
-    # Initialization functions ---------------------------------------------------------------------------------------------
+    # Initialization functions 
     def initialize_aberrations(self):
         self.aberrations = probe_tools.get_target_aberrations(self.microscope_name, self.optics['accelerating_voltage'])
         self.aberrations['reciprocal_FOV'] = reciprocal_FOV = 150*1e-3 # warning for these two lines - play heavily into probe calculations and all fft convolutions
@@ -236,7 +231,7 @@ class DTSTEM(BaseMicroscope):
         return
 
 
-    # Callable functions ---------------------------------------------------------------------------------------------------
+    # Callable functions
     def connect(self, ip, port):
         print('Connected to Digital Twin')
         return
@@ -256,8 +251,9 @@ class DTSTEM(BaseMicroscope):
             if self.optics['fov'] is None:
                 raise ValueError('Field of view not set, run microscope.set_field_of_view()')
 
-            # atomic resolution HAADF image ----------------------------------------------------------------------------
+            # atomic resolution HAADF image
             # right now this works for ~10<fov<200
+            # change this to match pystemsim
             if self.optics['fov'] < 300: # Angstroms
                 if atoms is None:
                     # Use Al structure
@@ -282,7 +278,7 @@ class DTSTEM(BaseMicroscope):
                 return image
 
 
-            # Blob HAADF image -----------------------------------------------------------------------------------------
+            # Blob HAADF image
             elif self.optics['fov'] > 1000: # Angstroms, arbitrary for now
                 number_of_electrons = 100 * dwell_time # approximation here ***** Gerd
                 size = 512
@@ -302,28 +298,3 @@ class DTSTEM(BaseMicroscope):
 
             else:
                 raise ValueError('Field of view should be < 300 or > 1000 Angstroms')
-
-
-
-class RealSTEM():
-    def __init__(self):
-        print("This implementation cannot be included - the ThermoFisher code is not open source")
-        print("If your implementation with DTMicroscope wins a prize, we will take care of the integration to the real microscope")
-        print("Thank you!")
-
-
-    # add get spectrum at specific location
-    
-if __name__== "__main__":
-    mic = STEM()
-    mic._load_dataset("data/test_stem.h5")
-    mic._parse_dataset()
-    info_list = mic.get_dataset_info()
-    print(info_list)
-    ov_img = mic.get_overview_image(channel_key="Channel_000")
-    plt.imshow(ov_img)
-    plt.show()
-
-    #print(mic.get_overview_image())
-    
-
--- DTMicroscope/server/smart_proxy.py
+++ DTMicroscope/server/smart_proxy.py
@@ -3,6 +3,9 @@
 import sys
 import os
 
+import json
+import socket
+
 import numpy as np
 import Pyro5.api
 
@@ -11,6 +14,72 @@ sys.path.insert(0, "C:\\AE_future\\autoscript_1_14\\")
 import autoscript_tem_microscope_client as auto_script
 
 os.environ["PYRO_LOGLEVEL"] = "DEBUG"
+
+# integrate inside TEMServer?
+class CEOSacquisitionTCP:
+    def __init__(self, host="127.0.0.1", port=7072):
+        self.host = host
+        self.port = port
+        self._next_id = 1
+
+    def _send_recv(self, message: dict) -> dict:
+        # Convert to JSON and encode in UTF-8
+        json_msg = json.dumps(message, separators=(",", ":"))  # no spaces
+        payload = json_msg.encode("utf-8")
+        netstring = f"{len(payload)}:".encode("ascii") + payload + b","
+
+        with socket.create_connection((self.host, self.port), timeout=3000) as sock:  # 5 minutes
+            sock.sendall(netstring)
+
+            # Read until we hit a complete netstring (ends with b",")
+            buffer = b""
+            while not buffer.endswith(b","):
+                chunk = sock.recv(4096)
+                if not chunk:
+                    break  # Server closed connection
+                buffer += chunk
+
+        # Parse netstring: "length:payload,"
+        try:
+            length_str, rest = buffer.split(b":", 1)
+            length = int(length_str)
+            payload = rest[:length]
+            return json.loads(payload.decode("utf-8"))
+        except Exception as e:
+            print("Malformed netstring or response:", buffer)
+            raise e
+
+    def _run_rpc(self, method: str, params: dict = None):
+        if params is None:
+            params = {}
+        msg = {
+            "jsonrpc": "2.0",
+            "id": self._next_id,
+            "method": method,
+            "params": params,
+        }
+        self._next_id += 1
+        reply = self._send_recv(msg)
+        if "error" in reply:
+            raise RuntimeError(f"RPC Error: {reply['error']}")
+        return reply.get("result", {})
+
+    def run_tableau(self, tab_type="Standard", angle=18):
+        return self._run_rpc("acquireTableau", {"tabType": tab_type, "angle": angle})
+
+    def correct_aberration(self, name: str, value=None, target=None, select=None):
+        params = {"name": name}
+        if value:
+            params["value"] = list(value)
+        if target:
+            params["target"] = list(target)
+        if select:
+            params["select"] = select
+        return self._run_rpc("correctAberration", params)
+
+    def measure_c1a1(self):
+        return self._run_rpc("measureC1A1", {})
+
 
 def default_flu_camera(detector_dict):
     detector_dict['flu_camera'] = {'size': 512,
@@ -41,6 +110,7 @@ def serialize(array):
     dtype = str(array.dtype)
     return array_list, array.shape,  dtype
 
+# Should mirror the DTSTEMServer class in server_stem.py
 @Pyro5.api.expose
 class TEMServer(object):
     """Class to handle the array server"""
@@ -53,11 +123,25 @@ class TEMServer(object):
         self.microscope = auto_script.TemMicroscopeClient()
         self.connect_to_as()
         print('initialized')
-        
 
+        self.ab_corrector = None
+        self.connect_to_ceos()
+        
     def connect_to_as(self):
         ip = "127.0.0.1"
         self.microscope.connect(ip, port = 9095)
+
+    def connect_to_ceos(self):
+        self.ab_corrector = CEOSacquisitionTCP(host="10.46.217.241", port=9092)
+
+    def measure_c1a1(self):
+        return self.ab_corrector.measure_c1a1()
+
+    def correct_aberration(self, name: str, value=None, target=None, select=None):
+        return self.ab_corrector.correct_aberration(name=name, value=value, target=target, select=select)
+
+    def run_tableau(self, tab_type="Standard", angle=18):
+        return self.ab_corrector.run_tableau(tab_type=tab_type, angle=angle)
 
     def get_detectors(self):
         """Get the list of available detectors"""
